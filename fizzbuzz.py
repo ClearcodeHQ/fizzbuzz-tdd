099b2b087777a93df061946fd04007b3e61d2b8d
Firemark <marpiechula@gmail.com>
2017-01-21 19:51:00 +0100

    add new feature

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,12 @@
-def fizzbuzz(n):
+def fizzbuzz(n, fizz='fizz', buzz='buzz'):
     s = ''
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
-            s += 'fizzbuzz'
+            s += fizz + buzz
         elif i % 3 == 0:
-            s += 'fizz'
+            s += fizz
         elif i % 5 == 0:
-            s += 'buzz'
+            s += buzz
         else:
             s += str(i)
         s += '\n'
