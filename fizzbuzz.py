878d4f9b38909a864d9ed1aa96f5c1124af1ecfa
Firemark <marpiechula@gmail.com>
2017-01-21 19:23:16 +0100

    write code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,12 @@
 def fizzbuzz(n):
-    return ''
+    s = ''
+    for i in range(1, n + 1):
+        if i % 3 == 0:
+            s += 'fizz'
+        elif i % 5 == 0:
+            s += 'buzz'
+        else:
+            s += str(i)
+        s += '\n'
+    return s
+
