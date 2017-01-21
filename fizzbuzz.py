f82ef4c840b390904148d07c0ac824fc267dfbd1
Firemark <marpiechula@gmail.com>
2017-01-21 19:37:22 +0100

    correct code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
 def fizzbuzz(n):
     s = ''
     for i in range(1, n + 1):
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
+            s += 'fizzbuzz'
+        elif i % 3 == 0:
             s += 'fizz'
         elif i % 5 == 0:
             s += 'buzz'
