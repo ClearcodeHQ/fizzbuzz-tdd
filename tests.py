24eac5088736dbcfb112f8df135b4b51177b9338
Firemark <marpiechula@gmail.com>
2017-01-21 19:49:14 +0100

    write test

--- tests.py
+++ tests.py
@@ -21,3 +21,16 @@ def test_fizzbuzz_count_to_fifteen():
     assert (
         result == '1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz\n11\nfizz\n13\n14\nfizzbuzz\n'
     )
+
+
+def test_fizzbuzz_count_to_5_with_another_words():
+    result = fizzbuzz(5, fizz='ogorek', buzz='ziemniak')
+    assert result == '1\n2\nogorek\n4\nziemniak\n'
+
+
+def test_fizzbuzz_count_to_15_with_another_words():
+    result = fizzbuzz(15, fizz='ogorek', buzz='ziemniak')
+    assert (
+        result == '1\n2\nogorek\n4\nziemniak\nogorek\n7\n8\nogorek\nziemniak\n11\nogorek\n13\n14\nogorekziemniak\n'
+    )
+
