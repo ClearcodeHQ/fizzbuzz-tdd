412472ef23f0cbc122512ded98e1feacdbc8c641
Marek Piechula <m.piechula@clearcode.cc>
2017-01-22 20:33:18 +0100

    write super feature or bug

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,17 @@
 def fizzbuzz(n, fizz='fizz', buzz='buzz'):
-    s = ''
-    for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            s += fizz + buzz
-        elif i % 3 == 0:
-            s += fizz
-        elif i % 5 == 0:
-            s += buzz
-        else:
-            s += str(i)
-        s += '\n'
-    return s
+    return ''.join(fizzbuzz_list(n, fizz, buzz))
 
-def fizzbuzz_list(n):
-    return []
+
+def fizzbuzz_list(n, fizz='fizz', buzz='buzz'):
+    return [fizzbuzz_single(i, fizz, buzz) for i in range(1, n + 1)]
+
+
+def fizzbuzz_single(n, fizz='fizz', buzz='buzz'):
+    if i % 3 == 0 and i % 5 == 0:
+        return fizz + buzz
+    elif i % 3 == 0:
+        return fizz
+    elif i % 5 == 0:
+        return buzz
+    else:
+        return str(i)
