2c7031af67e89de41b1119ce83b923b602702a64
Marek Piechula <m.piechula@clearcode.cc>
2017-01-22 20:43:51 +0100

    correct code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,12 @@
 def fizzbuzz(n, fizz='fizz', buzz='buzz'):
-    return ''.join(fizzbuzz_list(n, fizz, buzz))
+    return '\n'.join(fizzbuzz_list(n, fizz, buzz)) + '\n'
 
 
 def fizzbuzz_list(n, fizz='fizz', buzz='buzz'):
     return [fizzbuzz_single(i, fizz, buzz) for i in range(1, n + 1)]
 
 
-def fizzbuzz_single(n, fizz='fizz', buzz='buzz'):
+def fizzbuzz_single(i, fizz='fizz', buzz='buzz'):
     if i % 3 == 0 and i % 5 == 0:
         return fizz + buzz
     elif i % 3 == 0:
