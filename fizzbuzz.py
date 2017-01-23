ae83347bfc259070bf144f7e30ad019924811353
Marek Piechula <m.piechula@clearcode.cc>
2017-01-23 09:02:07 +0100

    LOL dziala

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,16 @@
 def fizzbuzz(n, fizz='fizz', buzz='buzz'):
-    s = ''
-    for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            s += fizz + buzz
-        elif i % 3 == 0:
-            s += fizz
-        elif i % 5 == 0:
-            s += buzz
-        else:
-            s += str(i)
-        s += '\n'
-    return s
+    return '\n'.join(fizzbuzz_list(n, fizz, buzz)) + '\n'
+
+def fizzbuzz_list(n, fizz='fizz', buzz='buzz'):
+    return [fizzbuzz_single(i, fizz, buzz) for i in range(1, n+1)]
+
+def fizzbuzz_single(i, fizz='fizz', buzz='buzz'):
+    if i % 3 == 0 and i % 5 == 0:
+        return fizz + buzz
+    elif i % 3 == 0:
+        return fizz
+    elif i % 5 == 0:
+        return buzz
+    else:
+        return str(i)
 
